5402f4de6401af7a0acbe96debefc2ca497a395a
henriquedani <henriquedani172004@gmail.com>
2023-08-29 21:06:44 -0300

    Teste inicial Relogio

--- TDD1/src/test/RelogioTest.java
+++ TDD1/src/test/RelogioTest.java
@@ -1,5 +1,19 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class RelogioTest {
+	
+	@Test
+	public void testeConfigurarRelogio() {
+		Relogio relogioteste = new Relogio();
+		
+		relogioteste.configurarRelogio(14, 30, 45);
+		
+		assertEquals(14, relogioteste.getHoras());
+		assertEquals(30, relogioteste.getMinutos());
+		assertEquals(45, relogioteste.getSegundos());
+	}
 
 }
