7baf52a44842c9b34144e0a822db75c082f5fb81
Henrique Dani <henriquedani172004@gmail.com>
2023-08-29 21:24:06 -0300

    Implementando método configurarRelogio

--- TDD1/src/main/Relogio.java
+++ TDD1/src/main/Relogio.java
@@ -1,5 +1,38 @@
 package main;
 
 public class Relogio {
+	private int horas;
+	private int minutos;
+	private int segundos;
 
+	public Relogio() {
+		this.horas = 0;
+		this.minutos = 0;
+		this.segundos = 0;
+	}
+
+	public void configurarRelogio(int horas, int minutos, int segundos) {
+		if (horas >= 0 && horas < 24 && minutos >= 0 && minutos < 60 && segundos >= 0 && segundos < 60) {
+			this.horas = horas;
+			this.minutos = minutos;
+			this.segundos = segundos;
+		} else {
+			this.horas = 0;
+			this.minutos = 0;
+			this.segundos = 0;
+			
+		}
+	}
+
+	public int getHoras() {
+		return horas;
+	}
+
+	public int getMinutos() {
+		return minutos;
+	}
+
+	public int getSegundos() {
+		return segundos;
+	}
 }
--- TDD1/src/test/RelogioTest.java
+++ TDD1/src/test/RelogioTest.java
@@ -2,6 +2,7 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import main.Relogio;
 
 public class RelogioTest {
 	
